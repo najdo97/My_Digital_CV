25d0f91dfbb0ef4ab2d4b74484a73b9a080d7eab
Borjan Najdoski <najdoskib@gmail.com>
2024-07-04 17:27:06 +0200

    Adding text to About section

--- My_Digital_CV/src/MainPanel.jsx
+++ My_Digital_CV/src/MainPanel.jsx
@@ -6,17 +6,37 @@ function MainPanel() {
             <div id="section1" className="about-section">
                 <h1>About section</h1>
                 <p>
-                    Lorem ipsum dolor, sit amet consectetur adipisicing elit. Nesciunt, totam et exercitationem reiciendis tempore architecto magni, veritatis unde ut illum, porro rem consequatur dignissimos est laudantium nobis quam. In, molestias.
-                    Lorem ipsum dolor, sit amet consectetur adipisicing elit. Nesciunt, totam et exercitationem reiciendis tempore architecto magni, veritatis unde ut illum, porro rem consequatur dignissimos est laudantium nobis quam. In, molestias.
-                    Lorem ipsum dolor, sit amet consectetur adipisicing elit. Nesciunt, totam et exercitationem reiciendis tempore architecto magni, veritatis unde ut illum, porro rem consequatur dignissimos est laudantium nobis quam. In, molestias.
-                    Lorem ipsum dolor, sit amet consectetur adipisicing elit. Nesciunt, totam et exercitationem reiciendis tempore architecto magni, veritatis unde ut illum, porro rem consequatur dignissimos est laudantium nobis quam. In, molestias.
-                    Lorem ipsum dolor, sit amet consectetur adipisicing elit. Nesciunt, totam et exercitationem reiciendis tempore architecto magni, veritatis unde ut illum, porro rem consequatur dignissimos est laudantium nobis quam. In, molestias.
-                    Lorem ipsum dolor, sit amet consectetur adipisicing elit. Nesciunt, totam et exercitationem reiciendis tempore architecto magni, veritatis unde ut illum, porro rem consequatur dignissimos est laudantium nobis quam. In, molestias.
-                    Lorem ipsum dolor, sit amet consectetur adipisicing elit. Nesciunt, totam et exercitationem reiciendis tempore architecto magni, veritatis unde ut illum, porro rem consequatur dignissimos est laudantium nobis quam. In, molestias.
-                    Lorem ipsum dolor, sit amet consectetur adipisicing elit. Nesciunt, totam et exercitationem reiciendis tempore architecto magni, veritatis unde ut illum, porro rem consequatur dignissimos est laudantium nobis quam. In, molestias.
-                    Lorem ipsum dolor, sit amet consectetur adipisicing elit. Nesciunt, totam et exercitationem reiciendis tempore architecto magni, veritatis unde ut illum, porro rem consequatur dignissimos est laudantium nobis quam. In, molestias.
+                    Hello! My name is Borjan, and I am a highly motivated and dedicated professional with a robust work ethic and a fervent enthusiasm for learning. 
+                    This web application serves as my personal digital CV, where you can find detailed information about my skills, experience, and the projects I have undertaken.
                 </p>
-
+                
+                <p>
+                    My journey towards becoming a software developer was inspired by my interest in cryptocurrencies.
+                     I came to understand that cryptocurrencies are essentially code, not magical internet money, but rather distributed systems and software. 
+                     This insight ignited a deep curiosity within me to comprehend the mechanics behind these technologies. 
+                     I realized that much of our daily interactions are mediated by software, and that businesses leverage software to enhance their efficiency. 
+                     Learning software development, therefore, equates to learning how to optimize and maximize human effectiveness.
+                </p>
+                <p>
+                    These revelations led me to reconsider my initial decision to enroll in the Faculty of Electrical Engineering. 
+                    Recognizing it as a misstep, I chose to start anew by enrolling in the Faculty of Information and Computer Sciences in Skopje, 
+                    embracing this challenge with determination.
+                </p>
+                <p>
+                    Concurrently, I began working as a logistics specialist at Taskforce BPO. 
+                    Presently, I am balancing a full-time job with my studies at the Faculty of Information and Computer Technologies in Skopje. 
+                    This dual engagement is demanding but it continually hones my professional and personal development. 
+                    My role involves constant interaction with customers, managing client expectations, and navigating our corporate structure at Taskforce.
+                     Outside of work, I devote my free time to solving LeetCode problems and engaging in projects that push the boundaries of my programming capabilities.
+                </p>
+                <p>
+                    I possess a foundational understanding of core concepts in object-oriented programming, data structures and algorithms, and web development. 
+                    I am well-versed in a range of technologies, including Java, C++, HTML, CSS, JavaScript, React, and SQL.
+                    I invite you to explore my GitHub and LeetCode profiles, linked in the header of this website.
+                </p>
+                <p>
+                    Join me as I continue to delve into the boundless possibilities of technology and programming!
+                </p>
             </div>
             <hr></hr>
             <div id="section2" className="education-section">
